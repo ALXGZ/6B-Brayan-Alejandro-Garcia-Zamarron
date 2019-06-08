7f27463006fbf3028017dadb8b4c6b3bce79ac96
Nahomi <43542697+ALXGZ@users.noreply.github.com>
2019-06-07 21:22:21 -0500

    Login

---
+++ LoginSB/LoginSB/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace LoginSB
+{
+    class Login
+    {
+        #region "atributos"
+        string usuario;
+        string contraseña;
+        #endregion
+
+        #region "propiedades"
+        public string USUARIO
+        {
+            set { usuario = value; }
+            get { return usuario; }
+        }
+        public string CONTRASEÑA
+        {
+            set { contraseña = value; }
+            get { return contraseña; }
+        }
+        #endregion
+
+        #region "Funciones"
+        public bool validarLogin(string u, string c)
+        {
+            string cadena = "Server=localhost;Database=ser_ban_login;uid=root;pwd=;port=3306;";
+            MySqlConnection con = new MySqlConnection(cadena);
+            con.Open();
+            MySqlCommand cmd;
+            MySqlDataReader dr;
+
+            string sql = "SELECT * FROM usuarios WHERE usuario = '"+u+"' and contrasena ='" + c + "' ";
+            cmd = new MySqlCommand(sql, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+
+            if (dr.HasRows)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña no incorrectos");
+                return false;
+            }
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ LoginSB/LoginSB/Menu_ppal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSB
+{
+    public partial class Menu_ppal : Form
+    {
+        public Menu_ppal()
+        {
+            InitializeComponent();
+        }
+    }
+}
